1935ffae8c6e85787328d213f20511a9320b5e67
bnmjhk <msjcuhammet0510@hotmail.com>
2024-05-07 15:15:40 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function capitalizeWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
 }
