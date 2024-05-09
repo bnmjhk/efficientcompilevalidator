66ef37f553fa93f5fc19d3607e05c46a2b87bc10
bnmjhk <msjcuhammet0510@hotmail.com>
2024-05-10 05:40:44 +0700

    init

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function numDecodings(s) {
-  const dp = new Array(s.length + 1).fill(0);
-  dp[0] = 1;
-  dp[1] = s[0] === "0" ? 0 : 1;
-  for (let i = 2; i <= s.length; i++) {
-    const oneDigit = parseInt(s.substring(i - 1, i));
-    const twoDigits = parseInt(s.substring(i - 2, i));
-    if (oneDigit >= 1) {
-      dp[i] += dp[i - 1];
-    }
-    if (twoDigits >= 10 && twoDigits <= 26) {
-      dp[i] += dp[i - 2];
+function longestPalindromeSubseq(s) {
+  const n = s.length;
+  const dp = Array.from(Array(n), () => Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    dp[i][i] = 1;
+  }
+  for (let len = 2; len <= n; len++) {
+    for (let i = 0; i < n - len + 1; i++) {
+      const j = i + len - 1;
+      if (s[i] === s[j]) {
+        dp[i][j] = 2 + dp[i + 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
     }
   }
-  return dp[s.length];
+  return dp[0][n - 1];
 }
